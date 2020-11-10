61041db474deb92b9835e3d210690c55fd310946
unknown <mhudyx@gmail.com>
2020-11-10 18:20:46 +0100

    Create route files

--- server/server.js
+++ server/server.js
@@ -1,6 +1,10 @@
 import express from 'express';
 import config from './config';
 import mongoose from 'mongoose';
+import userRoute from './routes/user.route';
+import profileRoute from './routes/profile.route';
+import postRoute from './routes/post.route';
+import authRoute from './routes/auth.route';
 
 const app = express();
 
@@ -11,5 +15,10 @@ mongoose.connect(mongodbUrl, {
     useUnifiedTopology: true,
     useCreateIndex: true
 }).catch(error => console.log(error.reason));
+
+app.use('/api/user', userRoute);
+app.use('/api/profile', profileRoute);
+app.use('/api/post', postRoute);
+app.use('/api/auth', authRoute);
 
 app.listen(5000, () => { console.log("Server is running at http://localhost:5000") });
